748b8005cb9dbf5c2b4b78235f2c1a2dbda092a1
Jose Galisteo <ceritium@gmail.com>
2017-12-02 16:48:47 +0100

    accounts summary

---
+++ accounts.rb
@@ -0,0 +1,31 @@
+require "tty-table"
+require_relative "lib/client"
+
+require "pry"
+rest_api = Client.client
+TO = "EUR"
+
+table = TTY::Table.new header: ['CURRENCY', "AMOUNT", "EUROS"]
+sum = 0
+table << [nil, nil, "CASH"]
+
+rest_api.accounts.each do |account|
+  currency = account.currency
+  balance = account.balance.to_f
+  output = [currency, balance]
+
+  if currency != TO
+		product_id = "#{currency}-EUR"
+    ratio = rest_api.last_trade(product_id: product_id).price.to_f
+		br = balance * ratio
+    output << "%.2f" % br
+		sum = sum + br
+	else
+		output << ""
+  end
+
+	table << output
+end
+
+table << [nil, "SUM", "%.2f" % sum]
+puts table.render(:basic, alignment: :right)
